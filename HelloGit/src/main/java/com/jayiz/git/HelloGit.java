daedde8fbe024e5e19a5d3bbefa9f3119c1612e6
JayIz <2674587379@qq.com>
2021-04-21 22:30:58 +0800

    创建分支

--- HelloGit/src/main/java/com/jayiz/git/HelloGit.java
+++ HelloGit/src/main/java/com/jayiz/git/HelloGit.java
@@ -4,5 +4,7 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("更新一");
+        System.out.println("更新二");
+        System.out.println("创建分支");
     }
 }
